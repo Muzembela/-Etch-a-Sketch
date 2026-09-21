07a10da2547e5418b74b50869530ccb2a6c87c16
Manuel Muzembela <mmuzembe@student.42luanda.com>
2026-09-21 15:43:32 +0100

    project almost done, the mandatory elements was done, styles in development

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const grid = document.querySelector(".gridContainer");
 createGrid = () => {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement("div")
-        dispatchEvent.classList.add("square")
+        div.classList.add("square");
         grid.appendChild(div);
     }
 }
-
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(event)
+{
+    event.target.classList.replace("square", "color");
+});
 createGrid()
