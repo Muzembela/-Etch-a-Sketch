17e11a7711a382695d492b6024669938515b8b09
Manuel Muzembela <mmuzembe@student.42luanda.com>
2026-09-23 14:54:36 +0100

    backend of reset button done, working on frontend

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 const grid = document.querySelector(".gridContainer");
-const userInput = document.getElementById("quantity");
 const resetButton = document.querySelector(".reset")
 
-const createGrid = () => {
-    grid.style.setProperty("grid-template-columns", "repeat(16, 1fr)");
-    grid.style.setProperty("grid-template-rows", "repeat(16, 1fr)");
+const createGrid = (size = 16) => {
+    grid.innerHTML=""
+    grid.style.setProperty("grid-template-columns", `repeat(${size}, 1fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(${size}, 1fr)`);
 
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < size*size; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
         grid.appendChild(div);
     }
 };
 
+const getRandomColor = () => {
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 updateGrid = ()=>
 {
     grid.innerHTML="";
     grid.style.setProperty("grid-template-columns", `repeat(${userInput.value}, 1fr)`);
     grid.style.setProperty("grid-template-rows", `repeat(${userInput.value}, 1fr)`);
-    for(let i=0; i < userInput.value*userInput.value; i++) {
+    for(let i = 0; i < userInput.value*userInput.value; i++) {
         const div = document.createElement("div")
         div.classList.add("square");
         grid.appendChild(div);
@@ -29,11 +36,23 @@ updateGrid = ()=>
 
 grid.addEventListener("mouseover", function(event) {
     if (event.target.classList.contains("square")) {
-        event.target.classList.replace("square", "color");
+        event.target.style.backgroundColor = getRandomColor()
     }
 });
 
-userInput.addEventListener("input", updateGrid);
+resetButton.addEventListener("click", () => {
+    let userInput = prompt("Enter the number of squares per side (1 - 100):");
+
+    let size = parseInt(userInput);
+
+    if (size >= 1 && size <= 100) {
+        createGrid(size);
+    } else if (userInput !== null) {
+        alert("Please, insert a valid number.");
+    }
+});
+
+userInput.addEventListener("prompt", updateGrid);
 
 resetButton.addEventListener("click", function() {
     grid.innerHTML="";
@@ -44,4 +63,4 @@ resetButton.addEventListener("click", function() {
 });
 
 
-createGrid()
+createGrid(16)
