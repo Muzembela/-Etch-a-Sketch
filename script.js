bd9b97c265a108491bb1b4f7db6ab87ce6b2c889
Manuel Muzembela <mmuzembe@student.42luanda.com>
2026-09-23 14:19:44 +0100

    button to reset size of grid added and working at 70 percent

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 const grid = document.querySelector(".gridContainer");
+const userInput = document.getElementById("quantity");
+const resetButton = document.querySelector(".reset")
 
-createGrid = () => {
+const createGrid = () => {
+    grid.style.setProperty("grid-template-columns", "repeat(16, 1fr)");
+    grid.style.setProperty("grid-template-rows", "repeat(16, 1fr)");
+
     for (let i = 0; i < 256; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
+    }
+};
+
+updateGrid = ()=>
+{
+    grid.innerHTML="";
+    grid.style.setProperty("grid-template-columns", `repeat(${userInput.value}, 1fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(${userInput.value}, 1fr)`);
+    for(let i=0; i < userInput.value*userInput.value; i++) {
         const div = document.createElement("div")
         div.classList.add("square");
         grid.appendChild(div);
     }
-}
-const square = document.querySelector("div");
-square.addEventListener("mouseover", function(event)
-{
-    event.target.classList.replace("square", "color");
+    console.log(userInput.value);
+};
+
+
+grid.addEventListener("mouseover", function(event) {
+    if (event.target.classList.contains("square")) {
+        event.target.classList.replace("square", "color");
+    }
 });
+
+userInput.addEventListener("input", updateGrid);
+
+resetButton.addEventListener("click", function() {
+    grid.innerHTML="";
+    userInput.value="";
+    grid.style.setProperty("grid-template-columns", `repeat(16, 1fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(16, 1fr)`);
+    createGrid()
+});
+
+
 createGrid()
